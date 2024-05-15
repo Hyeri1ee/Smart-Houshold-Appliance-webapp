4c1fdb5d5bc97c30617d0206f7b15e875b718c60
Twiston <twiston7@proton.me>
2024-05-15 22:25:08 +0200

    Logging in works.

--- public/static/authentication-code-catcher.js
+++ public/static/authentication-code-catcher.js
@@ -4,45 +4,61 @@ const getCode = async () => {
 
   if (!searchParams.has("grant_type") || !searchParams.has("code")) {
     console.log("no grant type or no code")
-    // redirect(false);
+    redirect(false);
   }
 
   window.sessionStorage.auth = searchParams.get("code");
 
   const url = "https://api.home-connect.com/security/oauth/token"
+
   const grant_type = "authorization_code"
   const code = window.sessionStorage.auth;
   const client_id = "0C2C2EEDCB6888B979936AF90CA1AFC637165B64A7775F49C4B747AE7017C63A"
   const client_secret = "CE5C4965F19A0106E435B75E7B0F4EDAE0F56F36A12210B1B91FFD31A1D0C779"
 
-  const resp = await fetch(`${url}?grant_type=${grant_type}&code=${code}&client_id=${client_id}&client_secret=${client_secret}`, {
-    content_type: "application/x-www-form-urlencoded",
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-  });
-
-  if (!resp.ok || resp.statusCode !== 200) {
-    console.log("bad resp");
-    // redirect(false);
+  const req_data = {
+    "grant_type": grant_type,
+    "code": code,
+    "client_id": client_id,
+    'client_secret': client_secret,
   }
+
+  const resp = await fetch(
+    `${url}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(req_data).toString()
+    }
+  );
 
   let data;
   try {
     data = await resp.json();
   } catch (e) {
-    console.log("bad json")
-    // redirect(false);
+    redirect(false);
   }
-  console.log(data);
-  // redirect(true);
+
+  try {
+    window.sessionStorage.token = data.access_token;
+    window.localStorage.refresh_token = data.refresh_token;
+  } catch (e) {
+    redirect(false);
+    console.log("data invalid");
+    console.log(data);
+  }
+
+  redirect(true);
 }
 
-const redirect = (wasSuccessful) => {
-  if (wasSuccessful === false) {
-    window.location.replace("/login/failed");
+const redirect = async (wasSuccessful) => {
+  if (wasSuccessful === true) {
+    await new Promise(r => setTimeout(r, 1000));
+    window.location.replace("/");
   } else {
-
+    await new Promise(r => setTimeout(r, 1000));
+    window.location.replace("/login/failed");
   }
 }
 
