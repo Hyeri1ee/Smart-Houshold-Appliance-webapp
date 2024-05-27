dd5bc96dd5e8d40cbb93d83c7cce58c5ae9e9b3c
Hyeri1ee <haerizian10@gmail.com>
2024-05-27 22:56:05 +0200

    TSK4 edited with endpoint, and status

--- server/src/controller/loginController.ts
+++ server/src/controller/loginController.ts
@@ -19,9 +19,9 @@ router.post('/', async (req: Request, res: Response) => {
 
     //3. if exists, return success
     if (userData.length > 0) {
-      return res.status(200).json({ status: 200, message: 'user exists' });
+      return res.status(204).json({ status: 204, message: 'User logged in successfully' });
     }
-    return res.status(401).json({ status: 401, message: 'user dont exist' });
+    return res.status(422).json({ status: 422, message: 'Validation error' });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ status: 500, message: 'server error' });
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,13 @@ import * as dbConnect from "./db/db-connect"
 const router: Router = express.Router();
 const app = express();
 const port = 1338;
-
+dbConnect.handleConnection();
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send('Hello World!')
 });
 //routing section
-app.use("/login", require("./controller/loginController"));
+app.use("/api/login", require("./controller/loginController"));
 
 
 app.listen(port, () => {
