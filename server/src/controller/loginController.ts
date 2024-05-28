f1655f4d494287c067f7ba5d4de6a36b7b5e13f1
Hyeri1ee <haerizian10@gmail.com>
2024-05-28 18:00:01 +0200

    TSK4 token made-edit

--- server/src/controller/loginController.ts
+++ server/src/controller/loginController.ts
@@ -7,7 +7,7 @@ import { User } from '../db/entities/user';
 import { getDataSource } from '../db/db-connect';
 import { UserJwtPayload } from './jwt-helper';
 import dotenv from 'dotenv';
-
+import { Revoked_token } from '../db/entities/revoked_token';
 dotenv.config();
 
 const hashing = async (password: string) => {
@@ -17,41 +17,49 @@ const hashing = async (password: string) => {
   return hashedPassword;
 }
 
-
 export const checkUserExist = async (req: Request, res: Response): Promise<void> => {
   const { email, password } = req.body;
-
   try {
     // 1. find user by email
     const dataSource = await getDataSource(); // get data source
     const userRepository = dataSource.getRepository(User);
     const user = await userRepository.findOne({ where: { email } });
+
     //2-1. if user does not exist
-    if (!user)
-    {
+    if (!user) {
       res.status(401).json({ error: 'Invalid email or password' });
       return;
     }
+
     const hashedPassword = await hashing(password);
     const hashedUserPassword = await hashing(user.password);
+
     // 2-2. compare with hashed password
-    if (!(await bcrypt.compare(password,hashedUserPassword))) {
+    if (!(await bcrypt.compare(password, hashedUserPassword))) {
       res.status(401).json({ error: 'Invalid email or password' });
       return;
     }
-    // 3. JWT 토큰 발급
+
+    // 3. JWT token generation
     const payload: UserJwtPayload = {
       user_id: user.user_id,
       first_name: user.first_name,
       email: user.email
     };
-    const token = jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: '1h' });
+    const secretKey: string = process.env.JWT_SECRET_KEY || "jwt-secret-key";
+    const token = jwt.sign(payload, secretKey);
+    const generatedTime = new Date();
 
-    // 4. 성공 응답 반환
-    res.status(200).json({ "token" : token });
+    /*// 4. put in the revoked token table
+    const revokedTokenRepository = dataSource.getRepository(Revoked_token);
+    const revokedToken = new Revoked_token();
+    revokedToken.token = token;
+    await revokedTokenRepository.save(revokedToken); */
+
+    // 5. success response
+    res.status(200).json({ "token": token });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Server error' });
   }
 };
-
