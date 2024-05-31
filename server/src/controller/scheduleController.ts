08f1d1db3dbb0974ce54f8eed709af159741cc12
Hyeri1ee <haerizian10@gmail.com>
2024-05-31 17:43:57 +0200

    FR14-2 -> demo merge conflict solve

--- server/src/controller/scheduleController.ts
+++ server/src/controller/scheduleController.ts
@@ -5,8 +5,7 @@ import { getDataSource } from '../db/db-connect';
 import { Brackets } from 'typeorm';
 
 export const checkSchedule = async (req: Request, res: Response): Promise<void> => {
-  const { userId } = req.body; // 요청 본문에서 userId를 받아옴
-
+  const { userId } = req.body; 
   if (!userId) {
     res.status(400).json({ error: 'User ID is required' });
     return;
@@ -16,25 +15,25 @@ export const checkSchedule = async (req: Request, res: Response): Promise<void> 
     const dataSource = await getDataSource();
     const scheduleRepository = dataSource.getRepository(Schedule);
 
-    // 현재 시간과 요일을 가져옴
+   
     const currentDate = new Date();
     const weekdayNow = currentDate.getDay();
     const timeNow = `${currentDate.getHours()}:${String(currentDate.getMinutes()).padStart(2, '0')}:${String(currentDate.getSeconds()).padStart(2, '0')}`;
 
-    // 현재 실행 중인 스케줄 쿼리
+  
     const currentRunningData = await scheduleRepository.createQueryBuilder('s')
       .innerJoinAndSelect('s.times', 't')
-      .innerJoin('s.user', 'u') // 사용자 테이블을 명시적으로 조인
+      .innerJoin('s.user', 'u') 
       .where('u.user_id = :userId', { userId })
       .andWhere('s.weekday = :weekday', { weekday: weekdayNow })
       .andWhere('t.start_time <= :timeNow', { timeNow })
       .andWhere('t.end_time >= :timeNow', { timeNow })
       .getMany();
 
-    // 미래 실행 예정인 스케줄 쿼리
+   
     const futureRunningData = await scheduleRepository.createQueryBuilder('s')
       .innerJoinAndSelect('s.times', 't')
-      .innerJoin('s.user', 'u') // 사용자 테이블을 명시적으로 조인
+      .innerJoin('s.user', 'u') 
       .where('u.user_id = :userId', { userId })
       .andWhere('s.weekday >= :weekday', { weekday: weekdayNow })
       .andWhere(new Brackets(qb => {
@@ -44,7 +43,7 @@ export const checkSchedule = async (req: Request, res: Response): Promise<void> 
       }))
       .getMany();
 
-    // 결과를 클라이언트에게 반환
+   
     res.status(200).json({
       currentRunning: currentRunningData,
       futureRunning: futureRunningData,
